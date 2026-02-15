aac4fa23f71dfd3d3a324690a9c5caa78bbd20bb
mhitratali-cmd <261566216+itraxteam-prog@users.noreply.github.com>
2026-02-15 11:24:39 +0500

    extra files removal.

--- simple.ps1
+++
@@ -1,5 +0,0 @@
-Add-Type -AssemblyName System.Drawing
-$img = [System.Drawing.Bitmap]::new('c:\Users\wwwit\OneDrive\Desktop\Web Protos\v0-school-website-design\public\images\logo.jpeg')
-$img.MakeTransparent()
-$img.Save('c:\Users\wwwit\OneDrive\Desktop\Web Protos\v0-school-website-design\public\images\logo_final.png', [System.Drawing.Imaging.ImageFormat]::Png)
-$img.Dispose()
