3cf40c1cb1f5e1dbaf3595454fa9094a9d3bfd4b
mhitratali-cmd <261566216+itraxteam-prog@users.noreply.github.com>
2026-03-12 23:37:14 +0500

    speed insight added to the website

--- app/layout.tsx
+++ app/layout.tsx
@@ -44,7 +44,8 @@ export const viewport: Viewport = {
 import { AuthProvider } from '@/context/AuthContext';
 import { ThemeProvider } from '@/components/theme-provider';
 import { Toaster } from "@/components/ui/sonner";
-import { Analytics } from "@vercel/analytics/react"
+import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 export default async function RootLayout({
   children,
@@ -69,6 +70,7 @@ export default async function RootLayout({
           <Toaster />
         </ThemeProvider>
         <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   )
